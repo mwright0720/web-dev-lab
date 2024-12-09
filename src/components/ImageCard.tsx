1b357028f62e448bbed749726df4b1504c71d38c
Michael <45725311+mwright0720@users.noreply.github.com>
2024-12-09 18:26:01 -0500

    Fix styling for Project 1

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,14 +1,14 @@
 type Props = { name: string; image?: string };
 export default function ImageCard({ name, image }: Props) {
   return (
-    <div className="bg-emerald-500">
+    <div className="bg-emerald-500 pt-9">
 
         <figure>
-            <img src = {image} alt="An image of the character" className="size-28"></img>
+            <img src = {image} alt="An image of the character" className="size-60"></img>
 
         </figure>
 
-        <h1 className="font-bold text-white my-2 bg-emerald-700 p-1">{name}</h1>
+        <h1 className="font-bold text-xl text-white my-2 bg-emerald-700 p-1">{name}</h1>
 
 
     </div>
--- src/pages/Proj1Home.tsx
+++ src/pages/Proj1Home.tsx
@@ -3,12 +3,17 @@ import ImageCard from "../components/ImageCard";
 
 export default function Proj1Home() {
   return (
-    <div>
-      <nav> <NavLink to = "/">Home Page</NavLink> </nav>
-      <div className="flex gap-3 justify-evenly">
+    <div className="bg-indigo-400 h-screen flex flex-col">
+
+      <nav className="bg-slate-600"> <NavLink to = "/">Home Page</NavLink> </nav>
+
+      <div className="flex justify-center items-center text-6xl font-bold">Drag the characters</div>
+      <div className="flex gap-28 justify-center items-center  min-h-screen">
+      
       <ImageCard name="Kurumi Tokisaki" image="../src/assets/kurumi.png"/>
       <ImageCard name="Tohka Yatogami" image="../src/assets/tohka.png"/>
       <ImageCard name="Nia Honjo" image="../src/assets/nia.png"/>
+
       </div>
 
     </div>
