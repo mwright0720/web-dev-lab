7e4c14b1ffa6cde70887e89a7f9d3804bf066fef
Michael <45725311+mwright0720@users.noreply.github.com>
2024-12-22 18:01:20 -0500

    Added new state logic to Proj2Home.tsx
    Keeps track of quantity of each role selected for each class within one object.

--- src/pages/Proj2Home.tsx
+++ src/pages/Proj2Home.tsx
@@ -1,35 +1,60 @@
 import { NavLink } from "react-router";
 import ToggleImage from "../components/ToggleImage";
 import { useState } from "react";
-import * as doll from "../utils/GFLPNCData.tsx"
+import * as doll from "../utils/GFLPNCData.tsx";
 
 export default function Proj2Home() {
-  type Doll = {id: number, image: string, role: string, active: boolean}
-  const [toggledState, setToggledState] = useState<Doll[]>([doll.croque, doll.souchun, doll.millau]);
-
-  const handleToggle = (id: number) => {
-    setToggledState(prevState => prevState.map (
-      character => character.id === id ? {...character, active: !character.active} : character
-    ))
-
-  }
-  const guardContent = [
+  type Doll = { id: number; 
+    image: string; 
+    role: 'guard' | 'medic' | 'sniper' | 'specialist' | 'warrior' | string; 
+    active: boolean };
+  const [toggledState, setToggledState] = useState<Doll[]>([
     doll.croque,
     doll.souchun,
     doll.millau,
-  ];
+  ]);
 
+  const [roleCounts, setRoleCounts] = useState<{[key: string]: number}>({
+    guard: 0,
+    medic: 0,
+    sniper: 0,
+    specialist: 0,
+    warrior: 0,
+  });
+  const handleToggle = (id: number) => {
+    setToggledState((prevState) =>
+      prevState.map((character) =>
+        character.id === id
+          ? { ...character, active: !character.active }
+          : character
+      )
+    );
+    let role = toggledState[id].role;
+    let active = toggledState[id].active;
 
-   const guardItems = guardContent.map((character) => (
-      <td className="p-3" key={character.id}>
-        <ToggleImage
-          image={character.image}
-          role={character.role}
-          toggle={toggledState.find((doll) => doll.id === character.id)?.active || false}
-          onClick={() => handleToggle(character.id)}
-        />
-      </td>
-    ));
+    if (active) {
+    setRoleCounts({...roleCounts, [role]: roleCounts[role] - 1}) 
+  }
+    else if (!active) {
+      setRoleCounts({...roleCounts, [role]: roleCounts[role] + 1})
+    }
+
+  };
+
+  const guardContent = [doll.croque, doll.souchun, doll.millau];
+
+  const guardItems = guardContent.map((character) => (
+    <td className="p-3" key={character.id}>
+      <ToggleImage
+        image={character.image}
+        role={character.role}
+        toggle={
+          toggledState.find((doll) => doll.id === character.id)?.active || false
+        }
+        onClick={() => handleToggle(character.id)}
+      />
+    </td>
+  ));
 
   return (
     <div className="flex flex-col h-screen bg-red-100 gap-14">
@@ -87,7 +112,9 @@ export default function Proj2Home() {
         </tr>
       </table>
 
-      <p className="text-5xl text-center">You have</p>
+      <p className="text-5xl text-center">You have {roleCounts.guard} guards, {roleCounts.medic} medics, {roleCounts.sniper} snipers, 
+        {roleCounts.specialist} specialists, {roleCounts.warrior} warriors.
+      </p>
       <div className="flex border gap-2 p-2 border-red-800 bg-zinc-800">
         <div>Team member 1</div>
         <div>Team member 2</div>
